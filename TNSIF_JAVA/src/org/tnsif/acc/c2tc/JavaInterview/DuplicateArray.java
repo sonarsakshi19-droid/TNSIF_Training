92529ee700a3ce7f4e7b10b160766813498f8c28
My PC <My PC@Sakshi>
2026-07-28 09:14:54 +0530

    First commit

---
+++ TNSIF_JAVA/src/org/tnsif/acc/c2tc/JavaInterview/DuplicateArray.java
@@ -0,0 +1,22 @@
+package org.tnsif.acc.c2tc.JavaInterview;
+
+import java.util.Scanner;
+public class DuplicateArray {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+        int[] arr = {1, 2, 3, 2, 4, 1};
+
+        System.out.println("Duplicate elements:");
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    System.out.println(arr[i]);
+                    break;
+                }
+            }
+        }
+    }
+}
+
+
