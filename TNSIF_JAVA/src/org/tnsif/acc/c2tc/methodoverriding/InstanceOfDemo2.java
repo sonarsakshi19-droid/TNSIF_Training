b672deeabd903a637f9118f999872bd069f01348
My PC <My PC@Sakshi>
2026-07-30 10:40:07 +0530

    First commit

---
+++ TNSIF_JAVA/src/org/tnsif/acc/c2tc/methodoverriding/InstanceOfDemo2.java
@@ -0,0 +1,40 @@
+package org.tnsif.acc.c2tc.methodoverriding;
+
+
+class PaymentMethod {
+	void makePayment() {
+		System.out.println("Processing payment");
+	}
+}
+
+class CreditCard extends PaymentMethod {
+	void swipeCard() {
+		System.out.println("Swiping credit card");
+	}
+}
+
+class Paypal extends PaymentMethod {
+	void loginToPaypal() {
+		System.out.println("Logging into paypal");
+	}
+}
+
+public class InstanceOfDemo2 {
+
+	public static void main(String[] args) {
+		PaymentMethod payment = new PaymentMethod();
+
+		payment = new Paypal();
+
+		if (payment instanceof CreditCard) {
+			CreditCard cc = (CreditCard) payment;
+			cc.swipeCard();
+		} else if (payment instanceof Paypal) {
+			Paypal pp = (Paypal) payment;
+			pp.loginToPaypal();
+		}
+
+	}
+
+}
+
