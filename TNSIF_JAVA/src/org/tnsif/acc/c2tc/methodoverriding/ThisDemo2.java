54e9e1e19159ffaf93fe5d2e8500d9f72f021a57
My PC <My PC@Sakshi>
2026-07-30 10:21:09 +0530

    First commit

---
+++ TNSIF_JAVA/src/org/tnsif/acc/c2tc/methodoverriding/ThisDemo2.java
@@ -0,0 +1,27 @@
+package org.tnsif.acc.c2tc.methodoverriding;
+
+class ProductBottle {
+	double price;
+
+	ProductBottle(double price) {
+		this.price = price;
+	}
+
+	double CalculateDiscount() {
+		return this.price * 0.10;
+	}
+
+	double calculateFinalPrice() {
+		return this.price - this.CalculateDiscount();
+	}
+}
+public class ThisDemo2 {
+
+	public static void main(String[] args) {
+		ProductBottle product = new ProductBottle(400);
+		System.out.println("Final Price :" + product.calculateFinalPrice());
+
+	}
+
+}
+
