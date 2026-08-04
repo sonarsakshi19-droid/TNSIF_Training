55a18679850632ac116cb99b432c675a276f0ae2
My PC <My PC@Sakshi>
2026-08-04 09:53:06 +0530

    First commit

---
+++ TNSIF_JAVA/src/org/tnsif/acc/c2tc/scannerclass_bufferreader/ScannerClassDemo.java
@@ -0,0 +1,18 @@
+package org.tnsif.acc.c2tc.scannerclass_bufferreader;
+
+import java.util.Scanner;
+public class ScannerClassDemo {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your name");
+		String name = sc.nextLine();
+		System.out.println("Enter your age");
+		int age = sc.nextInt();
+		System.out.println("Name of the user:" + name);
+		System.out.println("AGe of the user:" + age);
+		sc.close();// TODO Auto-generated method stub
+
+	}
+
+}
