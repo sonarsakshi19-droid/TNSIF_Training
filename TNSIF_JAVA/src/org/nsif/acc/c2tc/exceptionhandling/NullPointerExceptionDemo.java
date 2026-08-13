e348579fa067cac8cf14dafbdcf62ab9ee964a23
My PC <My PC@Sakshi>
2026-08-13 21:59:18 +0530

    First commit

---
+++ TNSIF_JAVA/src/org/nsif/acc/c2tc/exceptionhandling/NullPointerExceptionDemo.java
@@ -0,0 +1,19 @@
+package org.nsif.acc.c2tc.exceptionhandling;
+
+public class NullPointerExceptionDemo {
+
+	public static void main(String[] args) {
+		String str = null;
+		try {
+			System.out.println(str.length());
+		} catch (NullPointerException e) {
+			System.out.println(e);
+		} finally {
+			System.out.println("No matterwhat this block will get executed");
+		}
+
+	}
+
+}
+
+
