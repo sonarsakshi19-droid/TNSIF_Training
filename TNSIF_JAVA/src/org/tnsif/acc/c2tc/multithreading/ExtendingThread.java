a1fcf67ba0826ed5a148108acc5b995e5b6dfbe6
My PC <My PC@Sakshi>
2026-08-27 10:18:38 +0530

    First commit

---
+++ TNSIF_JAVA/src/org/tnsif/acc/c2tc/multithreading/ExtendingThread.java
@@ -0,0 +1,49 @@
+package org.tnsif.acc.c2tc.multithreading;
+
+
+
+class Eclipse extends Thread
+{
+	@Override
+	public void run()
+	{
+		System.out.println("Eclipse id" + "  " + Thread.currentThread().getId());
+	}
+}
+
+class OneNote extends Thread
+{
+	@Override
+	public void run()
+	{
+		System.out.println("OneNote id" + "  " + Thread.currentThread().getId());
+	}
+}
+class Chrome extends Thread
+{
+	@Override
+	public void run()
+	{
+		System.out.println("chrome id" + "  " + Thread.currentThread().getId());
+	} 
+}
+
+
+public class ExtendingThread {
+
+	public static void main(String[] args) {
+		Eclipse obj=new Eclipse();
+		obj.start();
+//		OneNote obj1=new OneNote();
+//		obj1.start();
+//		Chrome obj2 =new Chrome();
+//		obj2.start();
+		for (int i=0;i<5;i++) {
+		System.out.println("Main method thread id is " + "  " + Thread.currentThread().getId());
+		
+	}
+
+}
+}
+
+
