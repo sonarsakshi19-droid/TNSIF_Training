02bd279c3581c21a6703ed04d684c77df508d286
My PC <My PC@Sakshi>
2026-08-27 10:30:57 +0530

    First commit

---
+++ TNSIF_JAVA/src/org/tnsif/acc/c2tc/multithreading/SleepMethod.java
@@ -0,0 +1,26 @@
+package org.tnsif.acc.c2tc.multithreading;
+
+
+class Sleep extends Thread {
+	@Override
+	public void run() {
+		for (int i = 0; i < 5; i++) {
+			System.out.println("sleep id" + "  " + Thread.currentThread().getId());
+			try {
+				sleep(2000); // 1 sec
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+		}
+	}
+}
+
+public class SleepMethod {
+
+	public static void main(String[] args) {
+		Sleep obj = new Sleep();
+		obj.start();
+	}
+
+}
+
